4851612d44e725573a1bacdd923d8bd6b6ae1233
jonyisi <jony243840766@gmail.com>
2026-05-03 13:03:44 +0800

    Initial Commit

---
+++ 3.0 home demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  3.0 home demo
+//
+//  Created by 陈智健 on 2026/5/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3.0 home demo/__0_home_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  __0_home_demoApp.swift
+//  3.0 home demo
+//
+//  Created by 陈智健 on 2026/5/3.
+//
+
+import SwiftUI
+
+@main
+struct __0_home_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
